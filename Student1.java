cd0c5394b71760a1d017769e4e36a35c506ed465
Sandesh Raj <76520809+sandeshraj62@users.noreply.github.com>
2021-08-08 22:06:51 +0530

    Add files via upload

---
+++ Student1.java
@@ -0,0 +1,20 @@
+package javaOOPS;
+
+public class Student1 {
+	int id;
+	String name;
+}
+ class Student2{
+	 public static void main(String args[])
+	 {
+		 Student s1= new Student();
+		 Student s2= new Student();
+		 s1.id=1;
+		 s1.name="Sandesh";
+		 s2.id=2;
+		 s2.name="Anmol";
+		 System.out.println(s1.id+""+s1.name);
+		 System.out.println(s2.id+""+s2.name);
+	 }
+
+}
