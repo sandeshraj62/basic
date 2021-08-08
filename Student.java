e890bb01777d434eef6483c2d6f59be9a9dc0de3
Sandesh Raj <76520809+sandeshraj62@users.noreply.github.com>
2021-08-08 22:05:52 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,16 @@
+package java_2;
+
+public class Student {
+	int id;
+	String name;
+}
+ class Student_1{
+	 public static void main(String args[])
+	 {
+		 Student  n1= new Student();
+		 n1.id=1;
+		 n1.name="anmol";
+		 System.out.println(n1.id+""+n1.name);
+	 }
+
+}
