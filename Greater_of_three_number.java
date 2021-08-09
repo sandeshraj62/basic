dc2ff94892276b7b13a84b24feda1af4dffce124
Sandesh Raj <76520809+sandeshraj62@users.noreply.github.com>
2021-08-09 23:08:48 +0530

    Add files via upload

---
+++ Greater_of_three_number.java
@@ -0,0 +1,14 @@
+package smart_Programming;
+// Greater of three number://
+public class Greater_of_three_number {
+	public static void main(String args[])
+	{
+	int a=10,b=20,c=30;
+	if(a>b && a>c)
+		System.out.println("A is the greatest number amongs them:");
+	else if(b>a && b>c)
+		System.out.println("B is the greatest number among them:");
+	else
+		System.out.println("C is the greatest number among them:");
+    }
+}
---
+++ Greater_of_two_number.java
@@ -0,0 +1,12 @@
+package smart_Programming;
+// Find Greater of two number://
+public class Greater_of_two_number {
+	public static void  main(String args[]) {
+		int a=12,b=10;
+		if(a>b)
+			System.out.println("A is greater than B");
+		else
+			System.out.println("B is greater than A");
+	}
+
+}
