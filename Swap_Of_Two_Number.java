fc710f71e0c87c533732828a61275a97118b438f
Sandesh Raj <76520809+sandeshraj62@users.noreply.github.com>
2021-08-10 07:03:28 +0530

    Add files via upload

---
+++ Swap_Of_Two_Number.java
@@ -0,0 +1,13 @@
+package smart_Programming;
+// Swaping of two number using third variable.. //
+public class Swap_Of_Two_Number {
+	public static void main(String args[]) {
+		int a=1,b=2,t;
+		t=a;
+		a=b;
+		b=t;
+		System.out.println("Value of A is:"+a);
+		System.out.println("Value of B is:"+b);
+	}
+
+}
---
+++ Swaping_of_two_number_without_using_third_variable.java
@@ -0,0 +1,13 @@
+package smart_Programming;
+// Swaping of two number without using third variable..//
+public class Swaping_of_two_number_without_using_third_variable {
+	public static void main(String args[]) {
+		int a=1,b=2;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("Value of A is:"+a);
+		System.out.println("Value od B is:"+b);
+	}
+
+}
