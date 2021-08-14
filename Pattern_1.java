225597d4756cca93f3b4a119458154a59dba7440
Sandesh Raj <76520809+sandeshraj62@users.noreply.github.com>
2021-08-14 20:37:21 +0530

    Add files via upload

---
+++ Pattern_1.java
@@ -0,0 +1,23 @@
+package star_pattern;
+ // *
+ // **
+ // ***
+ // ****
+ // *****
+ // ******
+   
+public class Pattern_1 
+{
+  public static void main(String args[])
+  {
+	  for(int i=1;i<=6;i++)
+	  {
+		  for(int j=1;j<=i;j++)
+		  {
+			  System.out.print("*");
+		  }
+		  System.out.println();
+	  }
+	 
+ }
+}
