eba8fa89aba7e0d8fc886a2ae7c1d79113af642b
Sandesh Raj <76520809+sandeshraj62@users.noreply.github.com>
2021-08-15 17:41:20 +0530

    Add files via upload

---
+++ Program_1.java
@@ -0,0 +1,16 @@
+package prep_practise;
+  // positive or negative number..//
+public class Program_1 {
+	public static void main(String args[])
+	{
+		int num =-2;
+		if( num>0) {
+			System.out.println("Positive Number");
+		//}else if( num =0) {
+		//	System.out.println("Number is ZERO");
+		}else if( num<0) {
+			System.out.println("Negative Number");
+		}
+	}
+
+}
