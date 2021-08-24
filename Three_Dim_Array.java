e0e699ea9d43ac4b9651bf36b79bbea81a170c0f
Sandesh Raj <76520809+sandeshraj62@users.noreply.github.com>
2021-08-24 14:03:11 +0530

    Add files via upload

---
+++ Three_Dim_Array.java
@@ -0,0 +1,9 @@
+package java_Array;
+// 3D Array
+public class Three_Dim_Array {
+	public static void main(String args[])
+	{
+		int[][][] a = {{{10,20,30},{40,50,60,70},{80,90,100}}};
+		System.out.println(a[1].length);
+ }
+}
