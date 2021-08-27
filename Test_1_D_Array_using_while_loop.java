e44e48c983880c09b6fd3de8fa4a603f5cf753df
Sandesh Raj <76520809+sandeshraj62@users.noreply.github.com>
2021-08-27 22:10:42 +0530

    Add files via upload

---
+++ Test_1_D_Array_using_while_loop.java
@@ -0,0 +1,18 @@
+package java_Array;
+// printing all element of array using while loop.
+public class Test_1_D_Array_using_while_loop 
+{
+	public static void main(String args[])
+	{
+		int[] a= {10,20,30};
+		int b=0;
+		while(b<a.length)
+		{
+			System.out.print(a[b]+" ");
+			b++;
+		}
+		//for(int i=0;i<a.length;i++)
+	}
+	
+
+}
